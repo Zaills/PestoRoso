179044714c8539055623a86b2b222ba6b29463b5
Raphael Vinour <raphael.vinour@gmail.fr>
2026-04-09 15:20:15 +0200

    add connectionComponent

---
+++ src/components/Home/ConnectionComponent.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { socket } from '@/socket.ts'
+import router from '@/router'
+import { RouterLink } from 'vue-router'
+
+const name = ref('')
+const room = ref('')
+const buttonText = ref('Create Room')
+
+function roomUpdated() {
+  if (room.value === '') buttonText.value = 'Create Room'
+  else buttonText.value = 'Join Room'
+}
+
+function joinRoom() {
+  if (name.value === '') {
+    router.push('home')
+    return
+  }
+  if (room.value === '') {
+    room.value = Math.random().toString(36).slice(2)
+  }
+
+  if (room.value === 'roomId') {
+    router.push('home')
+    return
+  }
+
+  console.log(name)
+  socket.connect()
+  socket.emit('join_room', { room: room.value, name: name.value })
+
+  socket.on('user_joined', (sName) => {
+    if (name.value === sName) router.push({ path: `/${room.value}/${name.value}` })
+  })
+}
+</script>
+
+<template>
+  <input v-model="name" placeholder="Name" />
+  <input v-model="room" placeholder="Room" @input="roomUpdated" />
+  <RouterLink
+    v-if="room != 'roomId' && name != ''"
+    to="roomId/name"
+    @click="joinRoom()"
+    class="joinButton"
+    >{{ buttonText }}</RouterLink
+  >
+  <button v-else class="joinButton" disabled>{{ buttonText }}</button>
+</template>
+
+<style scoped>
+.joinButton {
+}
+</style>
