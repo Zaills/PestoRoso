3c73a92975a6d404d461ffeb5a6059004b01f533
Raphael Vinour <raphael.vinour@gmail.fr>
2026-04-29 13:20:59 +0200

    update ConnectionComponent and add TU for it add HomeView TU

--- src/components/Home/ConnectionComponent.vue
+++ src/components/Home/ConnectionComponent.vue
@@ -1,48 +1,18 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { socket } from '@/socket.ts'
-import router from '@/router'
 import { RouterLink } from 'vue-router'
 
 const name = ref('')
 const room = ref('')
-const buttonText = ref('Create Room')
-
-function roomUpdated() {
-  if (room.value === '') buttonText.value = 'Create Room'
-  else buttonText.value = 'Join Room'
-}
-
-function joinRoom() {
-  if (name.value === '') {
-    router.push('home')
-    return
-  }
-  if (room.value === '') {
-    room.value = Math.random().toString(36).slice(2)
-  }
-
-  if (room.value === 'roomId') {
-    router.push('home')
-    return
-  }
-
-  console.log(name)
-  socket.connect()
-}
 </script>
 
 <template>
   <input v-model="name" placeholder="Name" />
-  <input v-model="room" placeholder="Room" @input="roomUpdated" />
-  <RouterLink
-    v-if="room != 'roomId' && name != ''"
-    :to="room + '/' + name"
-    @click="joinRoom()"
-    class="joinButton"
-    >{{ buttonText }}</RouterLink
-  >
-  <button v-else class="joinButton" disabled>{{ buttonText }}</button>
+  <input v-model="room" placeholder="Room" />
+  <RouterLink v-if="room != '' && name != ''" :to="room + '/' + name" class="joinButton">
+    Join Room
+  </RouterLink>
+  <button v-else class="joinButton" disabled>Join Room</button>
 </template>
 
 <style scoped>
--- src/components/__tests__/view/HomeView.test.ts
+++ src/components/__tests__/view/HomeView.test.ts
@@ -4,7 +4,6 @@ import { mount } from '@vue/test-utils'
 // @ts-expect-error
 import HomeView from '@/views/HomeView.vue'
 
-
 describe('Home View', () => {
   it('Should render properly ', () => {
     mount(HomeView)
